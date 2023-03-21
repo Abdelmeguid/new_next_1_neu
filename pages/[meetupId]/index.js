e7bf2e1bf448d096d9f2b7e7457d5c7b65510389
BEDO <siegen567@gmail.com>
2023-03-21 05:27:57 +0200

    66
    66

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -54,7 +54,7 @@ export async function getStaticProps(context) {
   const meetupsCollection = db.collection("MIG");
 
   const selectedMeetup = await meetupsCollection.findOne({
-    _id: new ObjectId(meetupId) || [],
+    _id: new ObjectId(meetupId) || undefined,
   });
 
   client.close();
